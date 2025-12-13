b2d79acac836aff782802d2e8718e4f1b29deea4
shezma1176 <aidahafseenshezma@gmail.com>
2025-12-13 12:37:57 +0600

    Shapes

---
+++ CreateCanvas.py
@@ -0,0 +1,10 @@
+import turtle
+
+turtle.Screen().bgcolor("Orange")
+
+sc = turtle.Screen()
+sc.setup(400, 300)
+
+turtle.title("Welcome to Turtle Window")
+
+turtle.done()
---
+++ polygons.py
@@ -0,0 +1,31 @@
+import turtle
+
+turtle.Screen().bgcolor("Green")
+
+board = turtle.Turtle()
+
+#Triangle
+
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+#Square
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+turtle.done()
