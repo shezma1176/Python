ee772b49f2f95cb6831ace21f7c2f649d4fbbef1
shezma1176 <aidahafseenshezma@gmail.com>
2025-12-13 13:08:43 +0600

    Projects

---
+++ CoversationWithStrings.py
@@ -0,0 +1,10 @@
+# Define three different sentences
+sentence1 = "My name is Aidah Afseen Shezma."
+sentence2 = "I am around 11 years old."
+sentence3 = "I am a student of class 6."
+
+# Concatenate the sentences
+combined = sentence1 + " " + sentence2 + " " + sentence3
+
+# Print the result
+print(combined)
