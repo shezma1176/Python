04039862a3d1ff7ba66b5d147018355221762dbe
shezma1176 <aidahafseenshezma@gmail.com>
2025-12-19 12:37:28 +0600

    Python

---
+++ OddEvenNumber.py
@@ -0,0 +1,8 @@
+number = int(input("Enter Number To Check"))
+print("Number To Be Checked;", number)
+
+if number%2==0 :
+    print("This Is An Even Number")
+
+else:
+    print("This is An Odd Number")
---
+++ PositiveNegativeNumber.py
@@ -0,0 +1,11 @@
+number = int(input("Enter Number To Check"))
+print("Number To Be Checked;", number)
+
+if number>0:
+    print("This Is A Positive Number")
+
+elif number<0:
+    print("This Is A Negative Number")
+
+else:
+    print("This Is A Neutral Number")
