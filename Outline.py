ddffef9501c84ff0f0747b8ac698fda0a1dbc9de
shezma1176 <aidahafseenshezma@gmail.com>
2025-12-19 12:56:08 +0600

    Outline

---
+++ Outline.py
@@ -0,0 +1,11 @@
+number = int(input("Enter Number To Check"))
+print("Number To Be Checked;", number)
+
+if number>0:
+    print("This Is A Natural Number")
+
+elif number<0:
+    print("This Is An Unnatural Number")
+
+else:
+    print("This Is A Neutral Number")
