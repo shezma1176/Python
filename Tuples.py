969191b8b007e6d14d1950abdabc99b37e9de289
shezma1176 <aidahafseenshezma@gmail.com>
2026-01-03 12:53:03 +0600

    tuple

---
+++ Tuples.py
@@ -0,0 +1,21 @@
+my_tuple=(1, 4, 5)
+my_tuple1=(1, 4.5, "class")
+my_tuple2=("Ayaat", "Aidah", "Coding")
+print(my_tuple)
+print(my_tuple1)
+print(my_tuple2)
+
+#len
+
+print(len(my_tuple))
+print(my_tuple2[0])
+print(my_tuple2[1])
+print(my_tuple2[2])
+
+my_tuple=("mouse", [8, 4, 2], [1, 2, 3])
+print(my_tuple)
+print(my_tuple[1][2])
+print(my_tuple2[0:2])
+
+for i in my_tuple2:
+    print(i)
